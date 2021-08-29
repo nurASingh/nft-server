da36870a9f3a6481b30f8d6c0f9352cbfbeb7391
Arun Kumar Singh <arunkumar.singh2@ihsmarkit.com>
2021-08-29 23:15:26 +0530

    with new tx

--- models/nft.js
+++ models/nft.js
@@ -10,21 +10,21 @@ var NFT = [{
 },
 {    
     "Contract":  "ST22QPESFJ8XKJDWR1MHVXV2S4NBE44BA944NS4D2.my-nft",
-    "TransactionId":  "0x72f24d53e4834a29360c8213baa315a2c61f22bd337cc4459cf846dfa3b90054",
+    "TransactionId":  "0x2e9ca7137149ccbd534e6bb8a6bd41f8d5136653ff33e92a2385e4daf0d1c53e",
     "CalledBy":  "ST22QPESFJ8XKJDWR1MHVXV2S4NBE44BA944NS4D2",
     "Fees":  "0.000154STX",
     "Nounce": 3,
-    "BlockHight": 6742 ,
-    "BlockHash" : "0x2fac5f8c80aa7332e331cb71b492070d358524283d55c5913921ccd7a4b7d178"
+    "BlockHight": 6840 ,
+    "BlockHash" : "0x489fc003a7e1755df5415ef1c61d58284d3a25c17f1bf7b367c90a742375786c"
 },
 {    
     "Contract":  "ST22QPESFJ8XKJDWR1MHVXV2S4NBE44BA944NS4D2.my-nft",
-    "TransactionId":  "0x72f24d53e4834a29360c8213baa315a2c61f22bd337cc4459cf846dfa3b90054",
+    "TransactionId":  "0x2c160e13e8c54a6f7e52da450dd3204d8ebdfcdb5a57e4fd3413826a049e1fef",
     "CalledBy":  "ST22QPESFJ8XKJDWR1MHVXV2S4NBE44BA944NS4D2",
     "Fees":  "0.000154STX",
     "Nounce": 3,
-    "BlockHight": 6742 ,
-    "BlockHash" : "0x2fac5f8c80aa7332e331cb71b492070d358524283d55c5913921ccd7a4b7d178"
+    "BlockHight": 6841 ,
+    "BlockHash" : "0x8abc3c14e0ac12e3c023e0ec08f75f961a360e672d6966f13a5b3f8342bd0660"
 }]
 
 
